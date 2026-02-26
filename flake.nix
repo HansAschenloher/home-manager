7fe6468b7eff9ba64ae0a409af5f6c066aac7f92
Johann Aschenloher <johann.aschenloher@hensoldt.net>
2026-02-26 08:37:57 +0100

    Fix nvim and stylix module import

--- flake.nix
+++ flake.nix
@@ -63,6 +63,8 @@
         roles = {
           dev = {
             imports = [
+              nixvim.homeModules.nixvim
+              stylix.homeModules.stylix
               nix-index-database.homeModules.nix-index
               ./roles/dev
             ];
