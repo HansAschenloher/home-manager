2a7d96496c773ba4799bb272a4ada9683639aeca
Johann Aschenloher <hansi.aschenloher@hensoldt.net>
2026-09-22 08:45:36 +0200

    Use bwraped neovim

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,7 +1,11 @@
 { inputs, ... }: {
   flake.modules.homeManager.cli = { ... }: {
     home.packages = [
-      inputs.nixvim-config.packages."x86_64-linux".default
+      inputs.nixvim-config.packages."x86_64-linux".neovim-offline
     ];
+
+    home.sessionVariables = {
+      EDITOR = "nvim";
+    };
   };
 }
